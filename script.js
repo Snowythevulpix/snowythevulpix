2b210d9246120ffbeb5e94fcc5e6e20841f3a7b9
Snowythevulpix <118809222+Snowythevulpix@users.noreply.github.com>
2023-04-11 13:07:13 +0000

    stuff

---
+++ script.js
@@ -0,0 +1,6 @@
+// Check if the current browser supports standalone mode
+if (window.navigator.standalone === true) {
+  // The app is running in standalone mode, hide browser UI
+  // and perform any other customizations as needed
+  // Example: document.body.style.backgroundColor = '#ff0000';
+}
